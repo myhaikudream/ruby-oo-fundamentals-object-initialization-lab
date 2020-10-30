8f6fffe2da702a27b71bb70dea76378687c313ac
Christina <myhaikudream@yahoo.com>
2020-10-29 22:19:46 -0400

    commit tree 5c36c6c9fd42c46276de1a4ae2c1f778cc48f76d

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+    def initialize(name= "Beyonce")
+        @name = name
+      end
+
+    def name=(name)
+      @name = name
+    end
+  
+    def name
+      @name
+    end
+    
+  end
+
+  beyonce = Person.new
+  beyonce.name = "Beyonce"
+
+  puts beyonce.name
